f09edcb9951feb55654b582fa03d425c966cd4d7
Spencer Price <spencer.price@viacustomers.com>
2023-07-17 09:45:32 -0600

    Website description

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Javascript Standards';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_DESCRIPTION = 'Defining Javascript Standards Since 1997';
