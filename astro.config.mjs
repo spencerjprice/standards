f49bfd5d8686a642696a21d8e552acc9d2423fee
spencerjprice <spencerjprice@gmail.com>
2023-07-16 20:07:26 -0600

    custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,5 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://spencerjprice.github.io',
-    base: '/standards',
+    site: 'https://javascriptstandards.com',
 });
